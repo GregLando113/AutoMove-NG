ef13545b945a980ea6e4ea253725bec35ae197e3
Greg L <greglando@gmail.com>
2023-03-12 21:09:18 -0700

    fix serialization load

--- src/Serialization.cpp
+++ src/Serialization.cpp
@@ -5,29 +5,49 @@
 
 namespace AutoMove
 {
+	namespace
+	{
+		const uint32_t kSerializationVersion = 101;
+		const uint32_t kOnMarkerChange = 'MARK';
+		const uint32_t kOnDialogue = 'DIAL';
+	}
 
 	void OnGameSave(SKSE::SerializationInterface* a_inf)
 	{
-		auto markerchange = Papyrus::GetOnCustomMarkerChangeEvent();
-		markerchange->Save(a_inf, 'MARK', 101);
-		auto dialogue = Papyrus::GetOnPlayerDialogueEvent();
-		dialogue->Save(a_inf, 'DIAL', 101);
+		Papyrus::GetOnCustomMarkerChangeEvent()->Save(a_inf, kOnMarkerChange, kSerializationVersion);
+		Papyrus::GetOnPlayerDialogueEvent()->Save(a_inf, kOnDialogue, kSerializationVersion);
 	}
 
 	void OnGameLoad(SKSE::SerializationInterface* a_inf)
 	{
-		auto markerchange = Papyrus::GetOnCustomMarkerChangeEvent();
-		markerchange->Load(a_inf);
-		auto dialogue = Papyrus::GetOnPlayerDialogueEvent();
-		dialogue->Load(a_inf);
+		uint32_t type;
+		uint32_t version;
+		uint32_t length;
+		while (a_inf->GetNextRecordInfo(type, version, length))
+		{
+			if (version != kSerializationVersion) {
+				// Data is out of date or from the SSE version
+				// Dont load.
+				continue;
+			}
+
+			switch (type) {
+			case kOnMarkerChange:
+				Papyrus::GetOnCustomMarkerChangeEvent()->Clear();
+				Papyrus::GetOnCustomMarkerChangeEvent()->Load(a_inf);
+				break;
+			case kOnDialogue:
+				Papyrus::GetOnPlayerDialogueEvent()->Clear();
+				Papyrus::GetOnPlayerDialogueEvent()->Load(a_inf);
+				break;
+			}
+		}
 	}
 
 
 	void OnGameRevert(SKSE::SerializationInterface* a_inf)
 	{
-		auto markerchange = Papyrus::GetOnCustomMarkerChangeEvent();
-		markerchange->Revert(a_inf);
-		auto dialogue = Papyrus::GetOnPlayerDialogueEvent();
-		dialogue->Revert(a_inf);
+		Papyrus::GetOnCustomMarkerChangeEvent()->Revert(a_inf);
+		Papyrus::GetOnPlayerDialogueEvent()->Revert(a_inf);
 	}
 }
