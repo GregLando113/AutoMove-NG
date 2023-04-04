8694caa7d4a4aa2edb6b2f2ada14037ed88c2eb6
Greg L <greglando@gmail.com>
2023-04-04 11:06:29 -0700

    more changes to generalize build files

--- scripts/mkrelease.py
+++ scripts/mkrelease.py
@@ -8,14 +8,15 @@ parser = argparse.ArgumentParser(prog='mkrelease',description='Utility to make a
 parser.add_argument('-v', '--version', required=True)
 parser.add_argument('-f', '--file', required=True)
 parser.add_argument('-n', '--name', required=True)
+parser.add_argument('-o', '--outfilename', required=True)
 
 args = parser.parse_args()
 distfile = os.path.abspath(os.path.join('dist', f'{args.name}.{args.version}.zip'))
 
-print(f'Making release distribution:\n\t{args.version=}\n\t{args.file=}\n\t{distfile=}')
+print(f'Making release distribution:\n\t{args.version=}\n\t{args.file=}\n\t{distfile=}\n\t{args.outfilename=}')
 try:
     os.mkdir('dist')
 except FileExistsError:
     pass
 with zipfile.ZipFile(distfile,'w') as zip:
-    zip.write(os.path.abspath(args.file),f'SKSE/Plugins/{args.name}.dll')
+    zip.write(os.path.abspath(args.file),f'SKSE/Plugins/{args.outfilename}.dll')
